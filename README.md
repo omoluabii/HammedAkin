23ef16be35cf348b3ce6e07fa5bd17dc447582b8
Hammed Akinyemi <hakra911@gmail.com>
2022-05-12 23:31:40 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#My name is Hammed Akinyemi, I was referred by Akure Tech Hub, This is Working with Coding Tools
---
+++ script.js
@@ -0,0 +1,54 @@
+// Tips
+
+let me = "learning";
+console.log(me);
+
+// Module 5 Assignment
+
+let quote = "If you believe, nothing is impossible";
+
+// Module 5 Assignment
+
+
+// Module 6 Assignment
+
+let x = "42" + 7;
+
+let y = "42" - 7;
+
+console.log(x)
+console.log(y)
+// Module 6 Assignment
+
+
+// Module 7 Assignment
+
+let coding = "Coding is trendy and fun";
+let coding2 = "Coding is trendy and fun";
+
+console.log(coding)
+console.log(coding2)
+// Module 7 Assignment
+
+
+// Module 8 Assignment 1
+
+let firstMeal = "Salad";
+
+let secondMeal = "Pasta";
+
+let thirdMeal = "Jollof Rice";
+
+let fourthMeal = "Ice Cream";
+
+// Module 8 Assignment 1
+
+
+// Module 8 Assignment 2
+
+// In the Doc File 
+
+// Module 8 Assignment 2
+
+let a = (9 * 6) / 3 + 2; 
+console.log(a)
